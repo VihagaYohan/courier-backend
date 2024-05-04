e5375fcbe9ec765c0811b10cfeb9e79aa73211c4
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-05-04 22:38:08 +0530

    Modified - code changes

--- routes/order.js
+++ routes/order.js
@@ -5,22 +5,21 @@ const {
   getAllOrdersForUser,
   getOrderStatusUpdate,
   getAllOrdersForRider,
-  updateOrder,
+  updateOrderStatus,
 } = require("../controllers/Order");
 const { protect } = require("../middleware/authHandler");
 
 const router = express.Router();
 
-router
-  .route("/")
-  .get(protect, getAllOrders)
-  .post(protect, addOrder)
-  .put(protect, updateOrder);
+router.route("/").get(protect, getAllOrders).post(protect, addOrder);
 
 router.route("/user/:id").get(protect, getAllOrdersForUser);
 
 router.route("/user/:id/rider").get(protect, getAllOrdersForRider);
 
-router.route("/status/:id").get(protect, getOrderStatusUpdate);
+router
+  .route("/status/:id")
+  .get(protect, getOrderStatusUpdate)
+  .put(protect, updateOrderStatus);
 
 module.exports = router;
